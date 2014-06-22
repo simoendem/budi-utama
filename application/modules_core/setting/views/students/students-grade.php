1786e2a36ffd7d1b268d4782b7d48ea690b0ff49
albertusatria <albert@icubeonline.com>
2014-06-22 16:15:25 +0700

    fix styling panel

--- application/modules_core/setting/views/students/students-grade.php
+++ application/modules_core/setting/views/students/students-grade.php
@@ -32,26 +32,31 @@
         <form id="daftarUlang" class="form-horizontal">
         <div class="panel-body">
 	          <div class="form-group">
-	            <label class="col-sm-3 control-label">Tahun Ajaran</label>
+	            <label class="col-sm-2 control-label">Tahun Ajaran</label>
 	            <div class="col-sm-2">
 	              <input type="text" name="grades[tahun-ajaran]" value="2014/2015" class="form-control" disabled/>
 	            </div>
 	          </div>
-			<div class="form-group">
-			  <label class="col-sm-3 control-label">Jenjang Sekolah <span class="asterisk">*</span></label>
-			  <div class="col-sm-3">
-			    <select style="margin-top:8px;" name="grades[siswa_jenjang]" id="jenjangSekolah" class="form-control" required>
-			     <option value="" selected="selected">Pilih jenjang sekolah</option>		                      					      
-			    </select>
-			    <label class="error" for="jenjangSekolah"></label>
-			  </div>
-			   <div class="col-sm-3">
-			    <select style="margin-top:8px;" name="grades[siswa_kelas]" id="jenjangKelas" class="form-control" required>
-			      <option value="" selected="selected">Pilih jenjang kelas</option>		                      					      
-			    </select>
-			    <label class="error" for="jenjangKelas"></label>
-			  </div>					  
-			</div> 	    					           
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Unit</label>
+					<div class="col-sm-5">
+						<select class="form-control input-sm mb15">
+						<option>Option 1</option>
+						<option>Option 2</option>
+						<option>Option 3</option>
+						</select>
+					</div>
+				</div>		    
+				<div class="form-group">
+					<label class="col-sm-2 control-label">Jenjang</label>
+					<div class="col-sm-5">
+						<select class="form-control input-sm mb15">
+						<option>Option 1</option>
+						<option>Option 2</option>
+						<option>Option 3</option>
+						</select>
+					</div>
+				</div>		    					           
         </div>
 		<div class="panel-footer">
 	        <button id="cariSiswa" class="btn btn-warning">Cari</button>
@@ -60,54 +65,48 @@
       </div><!-- panel -->      
 
       
-		<div class="col-md-12">
-		  <div class="panel panel-default">
-		    <div class="panel-heading">
-		      <div class="panel-btns">
-		        <a href="#" class="panel-close">&times;</a>
-		        <a href="#" class="minimize">&minus;</a>
-		      </div>
-		      <h4 class="panel-title">Students List</h4>
-		    </div>
-		    <form id="gradesSiswa" class="form-horizontal form-bordered">
-		    <div class="panel-body">
-		    	<div class="row">
-		    		<div class="col-md-5">
-						<div class="form-group">
-							<h5 class="subtitle">Siswa yang sudah terdaftar</h5>
-							<div class="input-group col-sm-12">
-								<select class="form-control input-sm" multiple="multiple" id="lstBox1">
-									<option><span>10001 --</span> Bhimasta</option>
-									<option>10002 --</span> Simon M</option>
-									<option>10003 --</span> Albertus</option>
-								</select>
-							</div>
-						</div>		    					           
-		    		</div>
-		    		<div class="col-md-2 action-grades">
-						<input class="btn-info btn" type="button" id="btnRight" value ="  >  "/>
-						<br/><input  class="btn-info btn" type="button" id="btnLeft" value ="  <  "/>
-		    		</div>
-		    		<div class="col-md-5">
-						<div class="form-group">
-							<h5 class="subtitle">Calon Siswa yang akan didaftarkan</h5>
-							<div class="input-group col-sm-12">
-								<select class="form-control input-sm" multiple="multiple" id="lstBox2">
-									<option><span>10004 --</span> Raden</option>
-									<option>10005 --</span> Megadewandanu</option>
-									<option>10006 --</span> Satria Yudha</option>
-								</select>
-							</div>
-						</div>		    				    		
-		    		</div>		    				    		
-		    	</div>
-		</div><!-- panel-body -->
-		<div class="panel-footer">
-	        <button id="cariSiswa" class="btn btn-success btn-block">Save</button>
-	    </div>		    
-		    </form>
-		  </div><!-- panel -->
-		</div><!-- col-md-12 -->
+	  <div class="panel panel-default">
+	    <div class="panel-heading">
+	      <div class="panel-btns">
+	        <a href="#" class="minimize">&minus;</a>
+	      </div>
+	      <h4 class="panel-title">Students List</h4>
+	    </div>
+	    <form id="gradesSiswa" class="form-horizontal form-bordered">
+	    <div class="panel-body">
+	    	<div class="row">
+	    		<div class="col-md-5">
+					<div class="form-group">
+						<label class="control-label">Siswa yang sudah terdaftar</label>
+						<div class="input-group col-sm-12">
+							<select class="form-control input-sm" multiple="multiple" id="lstBox1">
+								<option><span>10001 --</span> Bhimasta</option>
+								<option>10002 --</span> Simon M</option>
+								<option>10003 --</span> Albertus</option>
+							</select>
+						</div>
+					</div>		    					           
+	    		</div>
+	    		<div class="col-md-2 action-grades">
+					<input class="btn-info btn" type="button" id="btnRight" value ="  >  "/>
+					<br/><input  class="btn-info btn" type="button" id="btnLeft" value ="  <  "/>
+	    		</div>
+	    		<div class="col-md-5">
+					<div class="form-group">
+						<label class="control-label">Calon Siswa yang ingin didaftarkan</label>
+						<div class="input-group col-sm-12">
+							<select class="form-control input-sm" multiple="multiple" id="lstBox2">
+								<option><span>10004 --</span> Raden</option>
+								<option>10005 --</span> Megadewandanu</option>
+								<option>10006 --</span> Satria Yudha</option>
+							</select>
+						</div>
+					</div>		    				    		
+	    		</div>		    				    		
+	    	</div>
+	    </div><!-- panel-body -->
+	    </form>
+	  </div><!-- panel -->
         
                 
     </div><!-- contentpanel -->
@@ -157,54 +156,5 @@ $( document ).ready(function() {
         e.preventDefault();
         return false;
     });		
-});
-</script>
-<script type="text/javascript">
-$( document ).ready(function() {
-    var modelMakeJsonList = {"modelMakeTable" : 
-        [
-            {"modelMakeID" : "0","modelMake" : "Pilih jenjang sekolah"},        
-            <?php $no = 1; foreach ($ls_unit as $unit): ?>
-                {"modelMakeID" : "<?php echo $no ?>","modelMake" : "<?php echo $unit->unit ?>"},
-            <?php $no++; endforeach ; ?>
-        ]};
-var modelTypeJsonList = {
-
-    <?php $no = 1; foreach ($ls_unit as $unit): ?>
-
-      "<?php echo $unit->unit ?>" :
-        [
-            <?php for ($i = 1; $i <= $unit->jenjang; $i++) : ?>
-                {"modelTypeID" : "<?php echo $unit->id_unit ?>","modelType" : "<?php echo $i ?>"}
-              <?php if ($i + 1 <= $unit->jenjang) : ?>
-              ,
-              <?php endif; ?>
-            <?php endfor; ?>
-        ],
-    <?php $no++; endforeach ; ?>		
-		};
-    console.log( "ready!" );
-
-	//Now that the doc is fully ready - populate the lists   
-	//Next comes the make
-      var ModelListItems= "";
-      for (var i = 0; i < modelMakeJsonList.modelMakeTable.length; i++){
-        ModelListItems+= "<option value='" + modelMakeJsonList.modelMakeTable[i].modelMakeID + "'>" + modelMakeJsonList.modelMakeTable[i].modelMake + "</option>";
-      }
-      $("#jenjangSekolah").html(ModelListItems);
-    
-    var updateSelectSchoolBox = function(make) {
-        console.log('updating with',make);
-        var listItems= "";
-        for (var i = 0; i < modelTypeJsonList[make].length; i++){
-            listItems+= "<option value='" + modelTypeJsonList[make][i].modelTypeID + "'>" + modelTypeJsonList[make][i].modelType + "</option>";
-        }
-        $("select#jenjangKelas").html(listItems);
-    }
-   
-    $("select#jenjangSekolah").on('change',function(){
-        var selectedMake = $('#jenjangSekolah option:selected').text();
-        updateSelectSchoolBox(selectedMake);
-    });    		
 });
 </script>
